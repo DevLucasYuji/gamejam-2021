63974c169bb612d6322a7ce2f781a603dfff9e5e
lyoshimine <lucas.yuji@enjoei.com.br>
2021-12-28 17:35:42 -0300

    Fix hud from window

--- hud.lua
+++ hud.lua
@@ -7,13 +7,14 @@ function drawHud()
     love.graphics.setFont(font)
     
     local width = camX - (window.width / 2) + 32
-    love.graphics.print(scoreText, width, 16)
+    local height = camY - (window.height / 2) + 16
+    love.graphics.print(scoreText, width, height)
 
 
     local font = love.graphics.newFont(14)
     love.graphics.setFont(font)
     
-    love.graphics.print("Fase: "  .. GAMEMAP.currentLevel.phase, width, 54)
+    love.graphics.print("Fase: "  .. GAMEMAP.currentLevel.phase, width, height + 48)
     
     love.graphics.setColor(1, 1, 1, 1)
 end
