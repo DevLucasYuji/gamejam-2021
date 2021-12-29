9ba529a0863864bdb08d3d983ebd67c452baf9a0
lyoshimine <lucas.yuji@enjoei.com.br>
2021-12-29 10:35:04 -0300

    Add new assets and refactor main lua

--- main.lua
+++ main.lua
@@ -3,36 +3,34 @@ window = {}
 window.width = 1280
 window.height = 720
 
--- Import Libs
-cameraFile = require 'libraries/hump/camera'
-windfield = require 'libraries/windfield/windfield'
-sti = require 'libraries/Simple-Tiled-Implementation/sti'
-anim8 = require 'libraries/anim8/anim8'
-
-
--- Load world
-world = windfield.newWorld(0, 800, false)
-
 SCORE = 0
 
--- Import packages
-require 'game/game'
-require 'sound'
-require 'sprites'
-require 'hud'
-require 'platform'
-require 'danger'
-require 'enemy'
-require 'ruby'
-require 'jarvis'
-require 'player'
-require 'map'
-
--- Current Game state
-GAMESTATE = game.state.resume
-GAMEMAP = loadMap(level.one)
-
 function love.load()
+    -- Import Libs
+    cameraFile = require 'libraries/hump/camera'
+    windfield = require 'libraries/windfield/windfield'
+    sti = require 'libraries/Simple-Tiled-Implementation/sti'
+    anim8 = require 'libraries/anim8/anim8'
+
+    world = windfield.newWorld(0, 800, false)
+
+    -- Import packages
+    require 'game/game'
+    require 'sound'
+    require 'sprites'
+    require 'hud'
+    require 'platform'
+    require 'danger'
+    require 'enemy'
+    require 'ruby'
+    require 'jarvis'
+    require 'player'
+    require 'map'
+
+    -- Current Game state
+    GAMESTATE = game.state.resume
+    GAMEMAP = loadMap(level.two)
+
     -- init setup
         love.window.setTitle("Brando's adventure")
         love.window.setMode(window.width, window.height)
